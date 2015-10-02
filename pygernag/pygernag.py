be07b6868e7b2dcdd1b3f823c556f2b53e4242d3
RY <ryoung@spiderhop.net>
2015-10-02 14:43:35 -0500

    Minor changes

--- pygernag/pygernag.py
+++ pygernag/pygernag.py
@@ -6,21 +6,29 @@ import logging
 import os
 import requests
 
+from requests.exceptions import ConnectionError
+
 
 def _logger(log_file=None):
     """
     Basic logger.
     """
 
-    log = logging.getLogger()
-    log.setLevel(logging.WARNING)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    if log_file is not None:
-        log.addHandler(logging.FileHandler(log_file))
+    if log_file:
+        fh = logging.FileHandler(log_file)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
     else:
-        log.addHandler(logging.StreamHandler())
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
-    return log
+    return logger
 
 
 def _get_args():
@@ -46,6 +54,10 @@ def _get_args():
     parser.add_argument('--nagios-api',
                         help='Enter the API endpoint for Nagios',
                         default=os.environ.get('NAGIOS_API_ENDPOINT', None)
+                        )
+    parser.add_argument('--log-file',
+                        help='Log output to file',
+                        default=os.environ.get('PYGERNAG_LOG', None)
                         )
 
     return parser
@@ -97,7 +109,12 @@ def nag_pd_sync_services(args, logger):
     pd_subdomain = args.pagerduty_domain
     nagios_api = args.nagios_api
 
-    r_nagios = requests.get('http://{0}/state'.format(nagios_api))
+    try:
+        r_nagios = requests.get('http://{0}/state'.format(nagios_api))
+    except ConnectionError:
+        logger.error('Unable to contact Nagios API: {0} - Exiting'.format(nagios_api))
+        os.sys.exit(1)
+
     ff = json.loads(r_nagios.text)
 
     service_problems, host_problems, no_ack_nag = [], [], []
@@ -120,14 +137,14 @@ def nag_pd_sync_services(args, logger):
                 no_ack_nag.append(problem)
 
     if not no_ack_nag:
-        logger.warning('No current Nagios service problems found')
+        logger.warn('No current Nagios service problems found')
     if no_ack_nag:
-        logger.warning('Nagios service problems found:\t {0}'
+        logger.warn('Nagios service problems found:\t {0}'
                        .format(len(no_ack_nag)))
     if not host_problems:
-        logger.warning('No current Nagios host problems found')
+        logger.warn('No current Nagios host problems found')
     if host_problems:
-        logger.warning('Nagios host problems found: \t {0}'
+        logger.warn('Nagios host problems found: \t {0}'
                        .format(len(host_problems)))
 
     headers = {
@@ -166,13 +183,13 @@ def nag_pd_sync_services(args, logger):
                 service_problems.append(temp)
 
     if service_problems:
-        logger.warning('Matches between Nagios and PD: {0}'
-                       .format(_json_dump(service_problems)))
+        logger.warn('Matches Found: {0}'.format(_json_dump(service_problems)))
 
     ack_in_nagios, ack_in_nagios_host = None, None
 
     # Check service problems for an acknowledgment
     # and ack back in Nagios where appropriate
+
     for problem in service_problems:
         if problem['status'] == 'acknowledged':
             if int(problem['problem_has_been_acknowledged']) == 0:
@@ -181,24 +198,24 @@ def nag_pd_sync_services(args, logger):
                     nagios_api,
                     service=problem['nagios_service']
                 )
+                logger.warn('Acknowledging Service problem in Nagios')
+                logger.warn(ack_in_nagios.json())
 
     # Do the same for Host problem alerts
     for host in host_problems:
         for pd_item in pd_incident_list_nag_trigger:
             if host == pd_item['trigger_summary_data']['HOSTNAME']:
                 ack_in_nagios_host = ack_alert(host, nagios_api)
-
-    if ack_in_nagios:
-        logger.warning(ack_in_nagios.json())
-    if ack_in_nagios_host:
-        logger.warning(ack_in_nagios_host.json())
+                logger.warn('Acknowledging Host problem in Nagios')
+                logger.warn(ack_in_nagios_host.json())
 
 
 def main():
-    nag_log = _logger()
-    nag_args = _get_args().parse_args()
 
-    if None in nag_args.__dict__.values():
+    nag_args = _get_args().parse_args()
+    nag_log = _logger(nag_args.log_file)
+
+    if None in nag_args.__dict__.values()[1:]:
         nag_log.warning(_get_args().print_help())
         os.sys.exit(1)
 
